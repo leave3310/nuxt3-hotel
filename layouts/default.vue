e007fa9393dda9997223560e0b168e29e4252997
leave3310 <sampisam3@gmail.com>
2024-08-05 16:40:36 +0800

    feat: 新增預設 layout

---
+++ layouts/default.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div>
+    <LayoutsHeaderBlock />
+    <main class="flex h-dvh flex-1 flex-col bg-neutral-100">
+      <slot />
+    </main>
+    <LayoutsFooterBlock />
+  </div>
+</template>
