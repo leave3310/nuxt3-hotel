1180ad98a5898756ad80f736fa572ea1854b1805
leave3310 <sampisam3@gmail.com>
2024-08-05 16:40:53 +0800

    feat: 新增只有 header 的 layout

---
+++ layouts/OnlyHeader.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div>
+    <LayoutsHeaderBlock />
+    <main class="flex h-[calc(100dvh-7.5rem)] flex-1 flex-col bg-neutral-100">
+      <slot />
+    </main>
+  </div>
+</template>
