76af9dd774cef58eaec4a6212a648f0748d3bf39
leave3310 <sampisam3@gmail.com>
2024-08-10 17:30:35 +0800

    feat: 新增首頁、房型列表、房型細節頁三頁router

---
+++ pages/rooms/index.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { getRooms } from "@/api/instances/rooms.ts";
+
+const { data } = await getRooms();
+</script>
+
+<template>
+  <div class="text-white">
+    <div v-for="room in data?.result" :key="room._id" class="flex">
+      <div>
+        <img :src="room.imageUrl" :alt="room.description">
+      </div>
+      <div>
+        {{ room.name }}
+      </div>
+    </div>
+  </div>
+</template>
